00dab3664639ee30482b4402eeaace66ad6e9d01
Scott Weinstein <sweinstein27@gmail.com>
2018-06-04 14:52:44 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,13 +11,13 @@ require 'pry'
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
-
   projects = {}
 
-kickstarter.css("li.project.grid_4").each do |project|
-  projects[project] = {}
-end
-projects
+  kickstarter.css("li.project.grid_4").each do |project|
+      title = project.css("h2.bbcard_name strong a").text
+      projects[title.to_sym] = {}
+    end
+
+    projects
+  end
 end
